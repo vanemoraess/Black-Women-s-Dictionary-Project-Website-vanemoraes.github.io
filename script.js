f3a3305e111aef3b5741f238eef5f7c72479de96
Vanessa Moraes <vanessamoraes@ufrj.br>
2023-01-27 13:57:37 -0300

    object + console.log

--- script.js
+++ script.js
@@ -1,29 +1,161 @@
 function carregaDicionario() {
 
-  var biografias = [
+  var biografias = {
 
-    "Marielle Franco",
+    bio01: {
 
-    "Rosa Parks",
+      nome: "Marielle Franco",
 
-    "Angela Davis",
+      imagem: "../dictionary/imagens/marielle-franco.png",
 
-    "Carolina de Jesus",
+      descricao:
 
-    "Lélia Gonzalez",
+        "Marielle Francisco da Silva, conhecida como Marielle Franco, foi uma socióloga, feminista, militante dos direitos humanos e política brasileira. Filiada ao Partido Socialismo e Liberdade (PSOL), elegeu-se vereadora do Rio de Janeiro para a Legislatura 2017-2020, durante a eleição municipal de 2016, com a quinta maior votação. Crítica da intervenção federal no Rio de Janeiro e da Polícia Militar, denunciava constantemente abusos de autoridade por parte de policiais contra moradores de comunidades carentes. Em 14 de março de 2018, foi assassinada a tiros",
 
-    "Conceição Evaristo",
+      citacao:
 
-    "Mãe Stella de Oxóssi",
+        "Quantos mais vão precisar morrer para que essa guerra aos pobres acabe?",
 
-    "Nina da Hora",
+    },
 
-    "Marina Silva",
+    bio02: {
 
-  ];
+      nome: "Lélia Gonzalez",
 
-  alert(biografias);
+      imagem: "../dictionary/imagens/lelia-gonzalez.png",
+
+      descricao: "Lélia Gonzalez foi uma intelectual, política, professora e antropóloga brasileira. Ajudou a fundar instituições como o Movimento Negro Unificado (MNU), o Instituto de Pesquisas das
+
+Culturas Negras (IPCN), o Coletivo de Mulheres Negras N'Zinga e o Olodum. Sua militância em defesa da mulher negra levou-a ao Conselho Nacional dos Direitos da Mulher (CNDM), no qual atuou de 1985 a 1989. Foi candidata a deputada federal pelo PT, elegendo-se primeira suplente. Nas eleições seguintes, em 1986, candidatou-se a deputada estadual pelo PDT, novamente elegendo-se suplente.",
+
+      citacao:
+
+        "A gente não nasce negro, a gente se torna negro. É uma conquista dura, cruel e que se desenvolve pela vida da gente afora.",
+
+    },
+
+    bio03: {
+
+      nome: "Angela Davis",
+
+      imagem: "../dictionary/imagens/angela-davis.png",
+
+      descricao:
+
+        "Angela Yvonne Davis é uma professora e filósofa socialista estado-unidense que alcançou notoriedade
+
+mundial na década de 1970 como integrante do Partido Comunista dos Estados Unidos, dos Panteras Negras, por sua militância pelos direitos das mulheres e contra a discriminação social e racial nos Estados Unidos e por ser personagem de um dos mais polêmicos e famosos julgamentos criminais da recente
+
+história dos Estados Unidos.",
+
+      citacao:
+
+        "Não aceito mais as coisas que não posso mudar, estou mudando as coisas que não posso aceitar.",
+
+    },
+
+    bio04: {
+
+      nome: "Conceição Evaristo",
+
+      imagem: "../dictionary/imagens/conceicao-evaristo.png",
+
+      descricao:
+
+        "Maria da Conceição Evaristo de Brito nasceu em Belo Horizonte, em 1946. De origem humilde, migrou para o Rio de Janeiro na década de 1970. Graduada em Letras pela UFRJ, trabalhou como professora da rede pública de ensino da capital fluminense. É Mestre em Literatura Brasileira pela PUC do Rio de Janeiro, com a dissertação Literatura Negra: uma poética de nossa afro-brasilidade (1996), e Doutora em Literatura Comparada na Universidade Federal Fluminense, com a tese Poemas malungos, cânticos irmãos (2011), na qual estuda as obras poéticas dos afro-brasileiros Nei Lopes e Edimilson de Almeida Pereira em confronto com a do angolano Agostinho Neto.",
+
+      citacao:
+
+        "O importante não é ser o primeiro ou primeira, o importante é abrir caminhos.",
+
+    },
+
+    bio05: {
+
+      nome: "Carolina de Jesus",
+
+      imagem: "../dictionary/imagens/carolina-de-jesus.png",
+
+      descricao:
+
+        "Carolina Maria de Jesus foi uma escritora brasileira, conhecida por seu livro Quarto de Despejo:
+
+Diário de uma Favelada publicado em 1960. Considerada uma das primeiras e mais importantes escritoras negras do Brasil. A autora viveu boa parte de sua vida na favela do Canindé, na zona norte de São Paulo, sustentando a si mesma e seus três filhos como catadora de papéis. Em 1958 tem seu diário
+
+publicado sob o nome Quarto de Despejo, com auxílio do jornalista Audálio Dantas. O livro fez um enorme sucesso e chegou a ser traduzido para quatorze línguas.",
+
+      citacao:
+
+        "Ah, comigo o mundo vai modificar-se. Não gosto do mundo como ele é.",
+
+    },
+
+    bio06: {
+
+      nome: "Rosa Parks",
+
+      imagem: "../dictionary/imagens/rosa-parks.png",
+
+      descricao:
+
+        "Rosa Louise McCauley, mais conhecida por Rosa Parks, foi uma costureira negra norte-americana, símbolo do movimento dos direitos civis dos negros nos Estados Unidos. Ficou famosa, em 1º de dezembro de 1955, por ter-se recusado frontalmente a ceder o seu lugar no ônibus a um branco, tornando-se o estopim do movimento que foi denominado boicote aos ônibus de Montgomery e posteriormente viria a marcar o início da luta antissegregacionista.",
+
+      citacao:
+
+        "⁠Você nunca deve ter medo do que está fazendo quando está certo.",
+
+    },
+
+    bio07: {
+
+      nome: "Mãe Stella de Oxóssi",
+
+      imagem: "../dictionary/imagens/mãe-stella-de-oxóssi.png",
+
+      descricao:
+
+        "Mãe Stella de Oxóssi, cujo nome de batismo era Francisca Gonçalves, nasceu em Salvador, Bahia, em 1925. Ela foi iniciada no candomblé aos 14 anos de idade, após visitar o terreiro de sua tia. Ela dedicou a vida a praticar e ensinar a religião, tornando-se uma das mais respeitadas e influentes ialorixás (mães de santo, chefe de um terreiro) do país. Ela fundou o terreiro Ilê Axé Opô Afonjá, em Salvador, e liderou diversas campanhas em defesa dos direitos dos praticantes de religiões de matriz africana, lutando contra a discriminação e a intolerância religiosa. Ela também foi uma defensora dos direitos das mulheres e dos direitos humanos. É considerada uma líder importante na valorização e promoção da cultura negra no Brasil, sua liderança foi fundamental para o reconhecimento e valorização das religiões de matriz africana no país. Ela faleceu em Salvador, Bahia, em 2019, aos 93 anos de idade.",
+
+      citacao:
+
+        "Criar desculpas para os próprios atos é a melhor maneira de permanecer no erro.",
+
+    },
+
+    bio08: {
+
+      nome: "Nina da Hora",
+
+      imagem: "../dictionary/imagens/nina-da-hora.png",
+
+      descricao:
+
+        "Nina da Hora é uma cientista da computação, pesquisadora e ativista brasileira, formada pela Pontifícia Universidade Católica do Rio de Janeiro (PUC-Rio). Ela é pesquisadora de novas possibilidades na área de pensamento computacional, e criou duas iniciativas, o Computação da Hora e o Ogunhê, para promover a educação e a ciência. Ela é professora, colunista da revista MIT Technology Review, integrante do Conselho de Segurança do TikTok, e participante da comissão de transparência das eleições brasileiras de 2022. Ela é conhecida por sua atuação como HackerAntirracista e foi incluída na lista Forbes 30 Under 30 da Forbes Brasil em 2021.</p>",
+
+      citacao:
+
+        "Vieses raciais, de gênero ou de qualquer outro tipo,por exemplo, não serão corrigidos com mais dados, mas sim com reflexão e pensamento crítico, pois se tratam de vieses humanos encontrados na sociedade.",
+
+    },
+
+    bio09: {
+
+      nome: "Marina Silva",
+
+      imagem: "../dictionary/imagens/marina-silva.png",
+
+      descricao:
+
+        "Marina Silva é uma política e ambientalista brasileira conhecida por sua luta pela defesa dos direitos das minorias e pela proteção do meio ambiente. Nasceu em uma família de agricultores no Acre e cresceu em condições de extrema pobreza. Apesar disso, conseguiu se formar como professora e começou a trabalhar como funcionária pública. Ao longo dos anos, destacou-se por sua dedicação à luta pela preservação da Amazônia e pelos direitos dos povos indígenas. Em 1989, juntou-se ao Partido dos Trabalhadores (PT) e rapidamente chamou atenção por sua liderança ambientalista. Foi eleita senadora em 1994 e, posteriormente, ministra do Meio Ambiente no governo Lula. Durante sua gestão, implementou medidas para proteger a Amazônia e reduzir a degradação ambiental no Brasil. Em 2010, tornou-se a primeira mulher negra a disputar a presidência do Brasil. Além de sua atuação política, Marina Silva é reconhecida por sua luta pela igualdade racial e de gênero, tendo falado abertamente sobre suas próprias experiências de discriminação e trabalhado para promover a inclusão de mulheres e pessoas negras em cargos de liderança.",
+
+      citacao:
+
+        "O custo do cuidado é sempre menor do que o custo do reparo.",
+
+    },
+
+  };
+
+  console.log(biografias);
 
 }
-
-carregaDicionario();
